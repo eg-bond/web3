2c041097b69055057ccfbd37120c634fcef2c6f0
eg-bond <eg-bond@yandex.ru>
2022-04-18 16:40:59 +0300

    events

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useReducer, useState } from 'react'
 import './App.css'
 import { bep20Abi } from './bep20Abi'
+import { erc20Abi } from './erc20Abi'
 import { createContract, useCurrentAccount, useWeb3 } from './web3Client'
 
 export const tokensReduser = (state, action) => {
@@ -23,11 +24,24 @@ export const tokensReduser = (state, action) => {
 function App() {
   let web3 = useWeb3()
   let selectedAccount = useCurrentAccount()
+  // native coin balance
   const [balance, setBalance] = useState(0)
 
-  let balances = JSON.parse(localStorage.getItem('tokens') || {})
+  const getBalance = async () => {
+    if (!!selectedAccount) {
+      const balance = await web3.eth.getBalance(selectedAccount)
+      setBalance(web3.utils.fromWei(balance))
+    }
+  }
+
+  let balances = JSON.parse(localStorage.getItem('tokens') || '{}')
 
   const [tokens, dispatch] = useReducer(tokensReduser, balances)
+
+  // fetch for address native balance if MM account changed
+  useEffect(() => {
+    getBalance()
+  }, [selectedAccount])
 
   useEffect(() => {
     localStorage.setItem('tokens', JSON.stringify(tokens))
@@ -38,6 +52,29 @@ function App() {
     bep20Abi,
     '0xC17c30e98541188614dF99239cABD40280810cA3'
   )
+  const riseContractV2 = createContract(
+    web3,
+    bep20Abi,
+    '0x0cD022ddE27169b20895e0e2B2B8A33B25e63579'
+  )
+
+  const nbuContract = createContract(
+    web3,
+    erc20Abi,
+    '0xEB58343b36C7528F23CAAe63a150240241310049'
+  )
+
+  const getEvents = () => {
+    nbuContract
+      .getPastEvents('Transfer', {
+        filter: { to: ['0x0fc00d2771762196a6ba66ec768764d2201afc08'] },
+        fromBlock: 'earliest',
+        toBlock: 'latest',
+      })
+      .then(console.log)
+  }
+
+  window.web3 = web3
   window.rise = riseContract
 
   const addToken = async address => {
@@ -48,11 +85,6 @@ function App() {
     balance = web3.utils.fromWei(balance, 'ether')
 
     dispatch({ type: 'ADD_TOKEN', symbol, balance })
-  }
-
-  const getBalance = async () => {
-    const balance = await riseContract.methods.balanceOf(selectedAccount).call()
-    setBalance(web3.utils.fromWei(balance))
   }
 
   const [inputValue, changeValue] = useState('')
@@ -81,6 +113,7 @@ function App() {
             </div>
           ))}
         </div>
+        <button onClick={() => getEvents()}>getEvents</button>
       </div>
     </div>
   )
